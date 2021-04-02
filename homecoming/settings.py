4010c70e63d04fa443b0a6449ad0ebb7cc9ccd50
bdsullivan123 <bdsullivan71@gmail.com>
2021-04-02 14:31:09 -0700

    added allowed host

--- homecoming/settings.py
+++ homecoming/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'z@bjq#tj^!rw8144rch)$_l9-get#vag8&ay73e2fvzf%*c#y-'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['https://swhomecoming.herokuapp.com/']
 
 
 # Application definition
@@ -119,3 +119,4 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
